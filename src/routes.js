63b1633a78f81a4d1a5be9c6253e941795539bc3
Broosmg <osamagaileaa@gmail.com>
2023-10-09 20:38:32 +0700

    First Commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addBooksHandler,
+  getAllBooksHandler,
+  getDetailedBookHandler,
+  editBookHandler,
+  deleteBookHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getDetailedBookHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookHandler,
+  },
+];
+
+module.exports = routes;
